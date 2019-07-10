2f8e1fc5251df1bed5024e2c23039dafc7aa7705
hunterszone <k.drenski91@gmail.com>
2019-07-11 02:17:23 +0300

    collidesWith method implemented

--- Gunman/src/main/java/gameDevelopment/EnemyEntity.java
+++ Gunman/src/main/java/gameDevelopment/EnemyEntity.java
@@ -1,19 +1,38 @@
 package gameDevelopment;
 
+import java.awt.Rectangle;
+
 public class EnemyEntity extends Entity {
+
+	private Rectangle hero = new Rectangle();
+	private Rectangle crosshair = new Rectangle();
+	private Rectangle enemy = new Rectangle();
+
 	public EnemyEntity(MySprite sprite, int x, int y) {
 		super(sprite, x, y);
 	}
 
 	@Override
-	public void collidedWith(HeroEntity other) {
-		System.out.println("Collision detected Enemy");
-		Game.notifyEnemiesHit(this, other);
+	public boolean collidesWith(Entity other) {
+		if (other instanceof HeroEntity) {
+			enemy.setBounds((int) x, (int) y, sprite.getWidth(), sprite.getHeight());
+			hero.setBounds((int) other.x, (int) other.y, other.sprite.getWidth(), other.sprite.getHeight());
+			System.out.println("Collision detected Enemy");
+			if (enemy.intersects(hero))
+				return true;
+		}
+		if (other instanceof Crosshair) {
+			enemy.setBounds((int) x, (int) y, sprite.getWidth(), sprite.getHeight());
+			crosshair.setBounds((int) other.x, (int) other.y, other.sprite.getWidth(), other.sprite.getHeight());
+			System.out.println("Collision detected Enemy");
+			if (enemy.intersects(crosshair))
+				return true;
+		}
+		return false;
 	}
 
 	@Override
 	public boolean remove(Entity other) {
-		return false;
+		return Game.notifyEnemiesHit(this, other);
 	}
-
 }
