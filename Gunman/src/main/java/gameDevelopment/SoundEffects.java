8e2debdf7d9b301c8ed0ffec1efa220ba271325f
hunterszone <k.drenski91@gmail.com>
2019-07-24 23:31:53 +0300

    LoadResourcesTest modified

---
+++ Gunman/src/main/java/gameDevelopment/SoundEffects.java
@@ -0,0 +1,65 @@
+package gameDevelopment;
+
+public enum SoundEffects {
+	BGMUSIC {
+		@Override
+		public String getSound() {
+			return "res/sounds/bgmusic.wav";
+		}
+	},
+	COLLECT {
+		@Override
+		public String getSound() {
+			return "res/sounds/collect.wav";
+		}
+	},
+	DENIED {
+		@Override
+		public String getSound() {
+			return "res/sounds/denied.wav";
+		}
+	},
+	DING {
+		@Override
+		public String getSound() {
+			return "res/sounds/ding.wav";
+		}
+	},
+	EXPLOSION {
+		@Override
+		public String getSound() {
+			return "res/sounds/explosion.wav";
+		}
+	},
+	FOOTSTEPS {
+		@Override
+		public String getSound() {
+			return "res/sounds/footsteps.wav";
+		}
+	},
+	HIGHSC {
+		@Override
+		public String getSound() {
+			return "res/sounds/highsc.wav";
+		}
+	},
+	HUH {
+		@Override
+		public String getSound() {
+			return "res/sounds/huh.wav";
+		}
+	},
+	MAGIC {
+		@Override
+		public String getSound() {
+			return "res/sounds/magic.wav";
+		}
+	},
+	QUACK {
+		@Override
+		public String getSound() {
+			return "res/sounds/quack.wav";
+		}
+	};
+	public abstract String getSound();
+}
--- Gunman/src/test/java/suite/tests/LoadResourcesTest.java
+++ Gunman/src/test/java/suite/tests/LoadResourcesTest.java
@@ -1,5 +1,6 @@
 package suite.tests;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -8,20 +9,20 @@ import java.nio.file.Paths;
 
 import org.junit.Test;
 
-import game_engine.Images;
-import sound_engine.SoundEffects;
+import gameDevelopment.Images;
+import gameDevelopment.SoundEffects;
 
 public class LoadResourcesTest {
 
 	@Test
 	public void testReadFile() {
-		assertTrue("Folder 'images' does not exist", Files.exists(Paths.get("images")));
-		assertTrue("Folder 'sounds' does not exist", Files.exists(Paths.get("sounds")));
+		assertTrue("Folder 'images' does not exist", Files.exists(Paths.get("res/images")));
+		assertTrue("Folder 'sounds' does not exist", Files.exists(Paths.get("res/sounds")));
 
-		if (Files.exists(Paths.get("images"))) {
+		if (Files.exists(Paths.get("res/images"))) {
 			for (int i = 0; i < Images.values().length; i++) {
-				assertTrue(Images.values()[i].toString() + " sound file does not exist",
-						Files.exists(Paths.get(Images.values()[i].getImg())));
+				assertFalse(Images.values()[i].toString() + " image file does not exist",
+						!Files.exists(Paths.get(Images.values()[i].getImg())));
 				assertNotEquals(Images.values()[i].toString() + " sound value is NULL", null,
 						Images.values()[i].getImg());
 				assertNotEquals(Images.values()[i].toString() + " sound value is empty", "",
@@ -29,10 +30,10 @@ public class LoadResourcesTest {
 			}
 		}
 
-		if (Files.exists(Paths.get("sounds"))) {
+		if (Files.exists(Paths.get("res/sounds"))) {
 			for (int i = 0; i < SoundEffects.values().length; i++) {
-				assertTrue(SoundEffects.values()[i].toString() + " sound file does not exist",
-						Files.exists(Paths.get(SoundEffects.values()[i].getSound())));
+				assertFalse(SoundEffects.values()[i].toString() + " sound file does not exist",
+						!Files.exists(Paths.get(SoundEffects.values()[i].getSound())));
 				assertNotEquals(SoundEffects.values()[i].toString() + " sound value is NULL", null,
 						SoundEffects.values()[i].getSound());
 				assertNotEquals(SoundEffects.values()[i].toString() + " sound value is empty", "",
