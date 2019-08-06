47d92addcbf3d65af1c58d5a967aab1840d5c746
hunterszone <k.drenski91@gmail.com>
2019-08-06 22:35:00 +0300

    Unit test to suite

--- PotOGold/src/test/java/suite/main/AllTests.java
+++ PotOGold/src/test/java/suite/main/AllTests.java
@@ -3,12 +3,17 @@ package suite.main;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import suite.tests.BombTest;
+import suite.tests.Gift1Test;
+import suite.tests.Gift2Test;
+import suite.tests.LepriconTest;
+
 @RunWith(Suite.class)
-@Suite.SuiteClasses({})
+@Suite.SuiteClasses({ BombTest.class, Gift1Test.class, Gift2Test.class, LepriconTest.class })
 
 public class AllTests {
 	public static Class<?>[] getClasses() {
-		Class<?>[] allClasses = {};
+		Class<?>[] allClasses = { BombTest.class, Gift1Test.class, Gift2Test.class, LepriconTest.class };
 		return allClasses;
 	}
 }
