b76f2af414c8398e795d23a3a85b2a286109217f
hunterszone <k.drenski91@gmail.com>
2019-08-24 21:14:38 +0300

    AllTests initial commit

--- SpaceBattle/src/test/java/suite/main/AllTests.java
+++ SpaceBattle/src/test/java/suite/main/AllTests.java
@@ -3,38 +3,12 @@ package suite.main;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import suite.tests.AlienTest;
-import suite.tests.BunkerBulletsTest;
-import suite.tests.BunkerTest;
-import suite.tests.CanonBallTest;
-import suite.tests.CollisionsTest;
-import suite.tests.ConsoleContentClassTest;
-import suite.tests.CrosshairTest;
-import suite.tests.DragonTest;
-import suite.tests.EvilHeadTest;
-import suite.tests.FireBallTest;
-import suite.tests.GoldTest;
-import suite.tests.HealthPackTest;
-import suite.tests.LoadResourcesTest;
-import suite.tests.MainClassTest;
-import suite.tests.MyShipTest;
-import suite.tests.SaveSignTest;
-import suite.tests.ShipMissileTest;
-import suite.tests.ShipRocketTest;
-
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ AlienTest.class, DragonTest.class, BunkerBulletsTest.class, BunkerTest.class, CanonBallTest.class,
-		CollisionsTest.class, ConsoleContentClassTest.class, CrosshairTest.class, DragonTest.class, EvilHeadTest.class,
-		FireBallTest.class, GoldTest.class, HealthPackTest.class, LoadResourcesTest.class, MainClassTest.class,
-		MyShipTest.class, SaveSignTest.class, ShipMissileTest.class, ShipRocketTest.class })
+@Suite.SuiteClasses({})
 
 public class AllTests {
 	public static Class<?>[] getClasses() {
-		Class<?>[] allClasses = { AlienTest.class, DragonTest.class, BunkerBulletsTest.class, BunkerTest.class,
-				CanonBallTest.class, CollisionsTest.class, ConsoleContentClassTest.class, CrosshairTest.class,
-				DragonTest.class, EvilHeadTest.class, FireBallTest.class, GoldTest.class, HealthPackTest.class,
-				LoadResourcesTest.class, MainClassTest.class, MyShipTest.class, SaveSignTest.class,
-				ShipMissileTest.class, ShipRocketTest.class };
+		Class<?>[] allClasses = {};
 		return allClasses;
 	}
 }
