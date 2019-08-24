cc85684aef7eb318fe3bb64660e35ae76d0e20f8
hunterszone <k.drenski91@gmail.com>
2019-08-24 22:38:29 +0300

    Tests added to suite

--- SpaceBattle/src/test/java/suite/main/AllTests.java
+++ SpaceBattle/src/test/java/suite/main/AllTests.java
@@ -3,12 +3,28 @@ package suite.main;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import suite.tests.AdvUfoTest;
+import suite.tests.EffectsTest;
+import suite.tests.GameEasyTest;
+import suite.tests.GameEndTest;
+import suite.tests.GameHardTest;
+import suite.tests.GameMediumTest;
+import suite.tests.GamePauseTest;
+import suite.tests.LoadIconTest;
+import suite.tests.PointsTest;
+import suite.tests.ShotMoonTest;
+import suite.tests.ShotShipTest;
+
 @RunWith(Suite.class)
-@Suite.SuiteClasses({})
+@Suite.SuiteClasses({ AdvUfoTest.class, EffectsTest.class, GameEasyTest.class, GameEndTest.class, GameHardTest.class,
+		GameMediumTest.class, GamePauseTest.class, LoadIconTest.class, PointsTest.class, ShotMoonTest.class,
+		ShotShipTest.class })
 
 public class AllTests {
 	public static Class<?>[] getClasses() {
-		Class<?>[] allClasses = {};
+		Class<?>[] allClasses = { AdvUfoTest.class, EffectsTest.class, GameEasyTest.class, GameEndTest.class,
+				GameHardTest.class, GameMediumTest.class, GamePauseTest.class, LoadIconTest.class, PointsTest.class,
+				ShotMoonTest.class, ShotShipTest.class };
 		return allClasses;
 	}
 }
