807eefdcbaf7dac76f77c550ac9de919e9bd292a
hunterszone <k.drenski91@gmail.com>
2020-05-28 17:12:53 +0300

    main menu update

--- EvilGalaxy/src/main/java/menu_states/MenuState.java
+++ EvilGalaxy/src/main/java/menu_states/MenuState.java
@@ -16,9 +16,13 @@ public class MenuState {
 		Font buttonFont = new Font("arial", Font.BOLD, 30);
 		g.setFont(buttonFont);
 
+		g.drawImage(Constants.LOAD_ASSETS.header, 428, 32, 352, 49, null);
+        g.drawImage(Constants.LOAD_ASSETS.headerBackground, 180, 0, 802, 105, null);
 		g.drawImage(Constants.LOAD_ASSETS.playGame, 410, 135, 388, 89, null);
 		g.drawImage(Constants.LOAD_ASSETS.loadGame, 410, 235, 388, 89, null);
 		g.drawImage(Constants.LOAD_ASSETS.settingsButton, 410, 335, 388, 89, null);
 		g.drawImage(Constants.LOAD_ASSETS.quitGame, 410, 435, 388, 89, null);
+		g.drawImage(Constants.LOAD_ASSETS.header, 428, 682, 352, 49, null);
+		g.drawImage(Constants.LOAD_ASSETS.headerBackground, 180, 650, 802, 105, null);
 	}
 }
