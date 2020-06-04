870a288dcf938a570365991b37ae775c0b8d15cf
hunterszone <k.drenski91@gmail.com>
2020-06-04 11:43:34 +0300

    mouse input fix

--- EvilGalaxy/src/main/java/menu_engine/MouseInputHandler.java
+++ EvilGalaxy/src/main/java/menu_engine/MouseInputHandler.java
@@ -168,8 +168,8 @@ public class MouseInputHandler implements MouseListener {
 					&& !(point.getY() >= 250 && point.getY() <= 300)
 					&& !(point.getY() >= 350 && point.getY() <= 400)
 					&& !(point.getY() >= 450 && point.getY() <= 500)
-					&& !(SettingsState.isOn && point.getY() >= 255 && point.getY() <= 255 + Constants.LOAD_ASSETS.myShip.getHeight(null))
-					&& !(SettingsState.isOn && point.getY() >= 355 && point.getY() <= 355 + Constants.LOAD_ASSETS.evilHead.getHeight(null))
+					&& !(SettingsState.isOn && point.getY() >= 280 && point.getY() <= 280 + Constants.LOAD_ASSETS.myShip.getHeight(null))
+					&& !(SettingsState.isOn && point.getY() >= 380 && point.getY() <= 380 + Constants.LOAD_ASSETS.evilHead.getHeight(null))
 					&& !(ManualState.isOn && point.getY() >= 475 && point.getY() <= 475 + Constants.LOAD_ASSETS.manual.getHeight(null)))
 			LoadSounds.menuMusic.loop();
 		}
