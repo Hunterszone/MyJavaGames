74e8fe7afda5da51166e2f1d4a95efbb0f6f9acd
hunterszone <k.drenski91@gmail.com>
2020-06-04 14:00:42 +0300

    labels fix

--- EvilGalaxy/src/main/java/game_engine/DrawScene.java
+++ EvilGalaxy/src/main/java/game_engine/DrawScene.java
@@ -400,7 +400,7 @@ public class DrawScene extends UpdateObjects {
 			}
 
 			if (GameMenuBar.autosave.isSelected() == true) {
-				g.drawString("Autosave: ON", 1000, 20);
+				g.drawString("Autosave: ON", 1020, 20);
 
 			}
 
