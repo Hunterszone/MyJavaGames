4ba70e22a4578d1f1c0b66d440d52317199042d8
kdrenski <k.drenski91@gmail.com>
2020-11-25 00:00:31 +0200

    code refactoring

--- EvilGalaxy/src/main/java/frames/SuggestionLabel.java
+++ EvilGalaxy/src/main/java/frames/SuggestionLabel.java
@@ -16,74 +16,74 @@ import javax.swing.border.LineBorder;
 
 class SuggestionLabel extends JLabel {
 
+	private static final long serialVersionUID = 1L;
+	private boolean focused = false;
+	private final JWindow autoSuggestionsPopUpWindow;
+	private final JTextField textField;
+	private final AutoSuggestor autoSuggestor;
+	private Color suggestionsTextColor, suggestionBorderColor;
 
- private static final long serialVersionUID = 1L;
- private boolean focused = false;
- private final JWindow autoSuggestionsPopUpWindow;
- private final JTextField textField;
- private final AutoSuggestor autoSuggestor;
- private Color suggestionsTextColor, suggestionBorderColor;
+	public SuggestionLabel(String string, final Color borderColor, Color suggestionsTextColor,
+			AutoSuggestor autoSuggestor) {
+		super(string);
 
- public SuggestionLabel(String string, final Color borderColor, Color suggestionsTextColor, AutoSuggestor autoSuggestor) {
-  super(string);
+		this.suggestionsTextColor = suggestionsTextColor;
+		this.autoSuggestor = autoSuggestor;
+		this.textField = autoSuggestor.getTextField();
+		this.suggestionBorderColor = borderColor;
+		this.autoSuggestionsPopUpWindow = autoSuggestor.getAutoSuggestionPopUpWindow();
 
-  this.suggestionsTextColor = suggestionsTextColor;
-  this.autoSuggestor = autoSuggestor;
-  this.textField = autoSuggestor.getTextField();
-  this.suggestionBorderColor = borderColor;
-  this.autoSuggestionsPopUpWindow = autoSuggestor.getAutoSuggestionPopUpWindow();
+		initComponent();
+	}
 
-  initComponent();
- }
+	private void initComponent() {
+		setFocusable(true);
+		setForeground(suggestionsTextColor);
 
- private void initComponent() {
-  setFocusable(true);
-  setForeground(suggestionsTextColor);
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent me) {
+				super.mouseClicked(me);
 
-  addMouseListener(new MouseAdapter() {
-   @Override
-   public void mouseClicked(MouseEvent me) {
-    super.mouseClicked(me);
+				replaceWithSuggestedText();
 
-    replaceWithSuggestedText();
+				autoSuggestionsPopUpWindow.setVisible(false);
+			}
+		});
 
-    autoSuggestionsPopUpWindow.setVisible(false);
-   }
-  });
+		getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, true), "Enter released");
+		getActionMap().put("Enter released", new AbstractAction() {
 
-  getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, true), "Enter released");
-  getActionMap().put("Enter released", new AbstractAction() {
+			private static final long serialVersionUID = 1L;
 
-   private static final long serialVersionUID = 1L;
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				replaceWithSuggestedText();
+				autoSuggestionsPopUpWindow.setVisible(false);
+			}
+		});
+	}
 
-   @Override
-   public void actionPerformed(ActionEvent ae) {
-    replaceWithSuggestedText();
-    autoSuggestionsPopUpWindow.setVisible(false);
-   }
-  });
- }
+	public void setFocused(boolean focused) {
+		if (focused) {
+			setBorder(new LineBorder(suggestionBorderColor));
+		} else {
+			setBorder(null);
+		}
+		repaint();
+		this.focused = focused;
+	}
 
- public void setFocused(boolean focused) {
-  if (focused) {
-   setBorder(new LineBorder(suggestionBorderColor));
-  } else {
-   setBorder(null);
-  }
-  repaint();
-  this.focused = focused;
- }
+	public boolean isFocused() {
+		return focused;
+	}
 
- public boolean isFocused() {
-  return focused;
- }
-
- private void replaceWithSuggestedText() {
-  String suggestedWord = getText();
-  String text = textField.getText();
-  String typedWord = autoSuggestor.getCurrentlyTypedWord();
-  String t = text.substring(0, text.lastIndexOf(typedWord));
-  String tmp = t + text.substring(text.lastIndexOf(typedWord)).replace(typedWord, suggestedWord);
-  textField.setText(tmp + "");
- }
+	private void replaceWithSuggestedText() {
+		String suggestedWord = getText();
+		String text = textField.getText();
+		String typedWord = autoSuggestor.getCurrentlyTypedWord();
+		String t = text.substring(0, text.lastIndexOf(typedWord));
+		String tmp = t + text.substring(text.lastIndexOf(typedWord)).replace(typedWord, suggestedWord);
+		textField.setText(tmp + "");
+	}
 }
