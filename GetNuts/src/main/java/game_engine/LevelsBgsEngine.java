5db970a1121cd56b41781198bed98ebe684a4973
kdrenski <k.drenski91@gmail.com>
2021-06-10 21:13:33 +0300

    check if file exists

--- GetNuts/src/main/java/game_engine/LevelsBgsEngine.java
+++ GetNuts/src/main/java/game_engine/LevelsBgsEngine.java
@@ -29,7 +29,10 @@ public class LevelsBgsEngine {
 	private static String fileName;
 
 	public static String readFile(String path, Charset encoding) throws IOException {
-		final byte[] encoded = Files.readAllBytes(Paths.get(path));
+		byte[] encoded = null;
+		if (new File(path).exists()) {
+			encoded = Files.readAllBytes(Paths.get(path));
+		}
 		return new String(encoded, encoding);
 	}
 
@@ -37,22 +40,21 @@ public class LevelsBgsEngine {
 		try {
 			final Stream<Path> files = Files.list(Paths.get("levels"));
 			final long filesCount = files.count();
-			IntStream.range(0, (int) filesCount)
-				.forEach(i -> {
-					String filePath = "levels/level" + i + ".txt";
-					Path path = Paths.get(filePath);
-					Path localFileName = path.getFileName();
-					fileName = localFileName.toString();
-					String levelString = null;
-					try {
-						levelString = readFile(filePath, Charset.defaultCharset());
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					map = stringToTwoDimCharArr(levelString, map);
-					levels.add(levelString);
-				});
+			IntStream.range(0, (int) filesCount).forEach(i -> {
+				String filePath = "levels/level" + i + ".txt";
+				Path path = Paths.get(filePath);
+				Path localFileName = path.getFileName();
+				fileName = localFileName.toString();
+				String levelString = null;
+				try {
+					levelString = readFile(filePath, Charset.defaultCharset());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				map = stringToTwoDimCharArr(levelString, map);
+				levels.add(levelString);
+			});
 			files.close();
 		} catch (final IOException e) {
 			// TODO Auto-generated catch block
@@ -66,18 +68,17 @@ public class LevelsBgsEngine {
 			final long imagesCount = images.count();
 			final int width = (int) Main.dim.getWidth();
 			final int height = (int) Main.dim.getHeight();
-			IntStream.range(0, (int) imagesCount)
-				.forEach(i -> {
-					File sourceImage = new File("backgrounds/bg" + i + ".jpg");
-					Image levelBG = null;
-					try {
-						levelBG = ImageIO.read(sourceImage);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					backgrounds.add(levelBG.getScaledInstance(width, height, Image.SCALE_SMOOTH));
-				});
+			IntStream.range(0, (int) imagesCount).forEach(i -> {
+				File sourceImage = new File("backgrounds/bg" + i + ".jpg");
+				Image levelBG = null;
+				try {
+					levelBG = ImageIO.read(sourceImage);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				backgrounds.add(levelBG.getScaledInstance(width, height, Image.SCALE_SMOOTH));
+			});
 			images.close();
 		} catch (final IOException e1) {
 			// TODO Auto-generated catch block
